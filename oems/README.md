be2c255e9c62259ef5716a87ce1b21f7050c0eac
Rahul Kumar <30971078+rahulkumarsahu@users.noreply.github.com>
2022-12-29 08:22:51 +0530

    First Commit

---
+++ oems/README.md
@@ -0,0 +1,9 @@
+# oems
+Office Employee Management System
+
+// Description :- OEMS app karta kya hai....
+
+// API DETAILS:- /api/v1/add -> GET -> kya karega
+                 ......./remove -> DELETE -> kya karega
+
+// Sample JSON Data....
---
+++ oems/oemsapp/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from oemsapp.models import Departments, Employees
+
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Departments
+        fields = ('DepartmentId', 'DepartmentName')
+
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employees
+        fields = ('EmployeeId', 'EmployeeName', 'Department', 'DateOfJoining', 'PhotoFileName')
